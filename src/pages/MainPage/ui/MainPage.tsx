366ae196792ce8e6efa6f3a3694b76e850ccf7b5
Mrfroxi <froxi340@mail.ru>
2024-03-02 20:26:11 +0300

    fix:deleted unUsed link

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const MainPage = () => {
   return (
-      <div> 
+      <div>
        MainPage
-      </div> 
+      </div>
   )
 }
 
